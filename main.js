4af6332eee8b4c0ff0702e21f5ce3ad32e9c347c
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-04-15 23:24:12 +0000

    issue-5-steer-car

--- main.js
+++ main.js
@@ -10,15 +10,33 @@ window.addEventListener('keydown', handleDirection);
 function handleDirection(event) {
   if (event.key === 'ArrowRight') {
     $car.className = 'image-container rotate-none';
+    if (carModel.carMoving === true) {
+      clearInterval(intervalID);
+      intervalID = setInterval(driveRight, 16);
+    }
   }
   if (event.key === 'ArrowDown') {
     $car.className = 'image-container rotate-right';
+    if (carModel.carMoving === true) {
+      clearInterval(intervalID);
+      intervalID = setInterval(driveDown, 16);
+    }
+
   }
   if (event.key === 'ArrowLeft') {
     $car.className = 'image-container rotate-opposite';
+    if (carModel.carMoving === true) {
+      clearInterval(intervalID);
+      intervalID = setInterval(driveLeft, 16);
+    }
   }
   if (event.key === 'ArrowUp') {
     $car.className = 'image-container rotate-left';
+    if (carModel.carMoving === true) {
+      clearInterval(intervalID);
+      intervalID = setInterval(driveUp, 16);
+    }
+
   }
   if (event.key === ' ' && carModel.carMoving === false) {
     intervalID = setInterval(startCar, 16);
@@ -30,6 +48,26 @@ function handleDirection(event) {
 }
 
 function startCar() {
+  carModel.x += 10;
+  $car.style.left = (carModel.x + 'px');
+}
+
+function driveDown() {
+  carModel.y += 10;
+  $car.style.top = (carModel.y + 'px');
+}
+
+function driveLeft() {
+  carModel.x -= 10;
+  $car.style.left = (carModel.x + 'px');
+}
+
+function driveUp() {
+  carModel.y -= 10;
+  $car.style.top = (carModel.y + 'px');
+}
+
+function driveRight() {
   carModel.x += 10;
   $car.style.left = (carModel.x + 'px');
 }
